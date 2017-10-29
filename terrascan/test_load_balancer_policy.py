7b6d1edd389c866602c0636455fcdbc3d4ff9430
Jason Tyler <jtclassified@gmail.com>
2017-10-29 12:22:03 -0400

    Init of test_load_balancer.py

---
+++ terrascan/test_load_balancer_policy.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+import terraform_validate
+from . import settings
+
+class TestEncryption(unittest.TestCase):
+
+    def setUp(self):
+        # Tell the module where to find your terraform configuration folder
+        self.path = os.path.join(
+            os.path.dirname(
+                os.path.realpath(__file__)), settings.TERRAFORM_LOCATION)
+        self.v = terraform_validate.Validator(self.path)
